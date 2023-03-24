6e9c290ab15df1bc141c5f725d246861047aae0c
SWADSOURCE <128734547+SWADSOURCE@users.noreply.github.com>
2023-03-24 20:10:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","13277434"))
-API_HASH = getenv("API_HASH","8444bed3061cda264cd6874de8fa45cc")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("BOT_TOKEN","5628899181:AAHSLZoVU55UwpBhT1-m6z87nijZ-XDBMDw")
-OWNER_ID = int(getenv("OWNER_ID","5566753847"))
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://szein:szein@cluster0.mw3zpgd.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN","t.me/S_325")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+MUST_JOIN = getenv("MUST_JOIN")
